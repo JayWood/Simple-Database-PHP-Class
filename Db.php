989427e7c0774266cf14357dc978e534ea7b458c
JayWood <jay@plugish.com>
2014-07-26 11:22:01 -0400

    Formatting fixes

--- Db.php
+++ Db.php
@@ -7,6 +7,7 @@ class Db {
 	/* Configuration */
 	/**
 	 * Configuration storage
+	 *
 	 * @var array
 	 */
 	protected static $config = array(
@@ -17,12 +18,13 @@ class Db {
 	);
 	/**
 	 * Get and set default Db configurations
+	 *
 	 * @uses   static::config
-	 * @param  string|array $key   [Optional] Name of configuration or hash array of configurations names / values
-	 * @param  mixed        $value [Optional] Value of the configuration
+	 * @param string|array $key   [Optional] Name of configuration or hash array of configurations names / values
+	 * @param mixed   $value [Optional] Value of the configuration
 	 * @return mixed        Configuration value(s), get all configurations when called without arguments
 	 */
-	static public function config ( $key = null, $value = null ) {
+	static public function config( $key = null, $value = null ) {
 		if ( ! isset( $key ) )
 			return static::$config;
 		if ( isset( $value ) )
@@ -35,22 +37,24 @@ class Db {
 	/* Static instances */
 	/**
 	 * Multiton instances
+	 *
 	 * @var array
 	 */
 	protected static $instance  = array();
 	protected static $arguments = array( 'driver', 'host', 'database', 'user', 'password' );
 	/**
 	 * Get singleton instance
+	 *
 	 * @uses   static::config
 	 * @uses   static::__construct
-	 * @param string $driver   [Optional] Database driver
-	 * @param string $host     [Optional] Database host
-	 * @param string $database [Optional] Database name
-	 * @param string $user     [Optional] User name
-	 * @param string $pass     [Optional] User password
+	 * @param string  $driver   [Optional] Database driver
+	 * @param string  $host     [Optional] Database host
+	 * @param string  $database [Optional] Database name
+	 * @param string  $user     [Optional] User name
+	 * @param string  $pass     [Optional] User password
 	 * @return Db Singleton instance
 	 */
-	static public function __callStatic ( $name, $config ) {
+	static public function __callStatic( $name, $config ) {
 		if ( isset( static::$instance[ $name ] ) )
 			return static::$instance[ $name ];
 		$config = array_merge(
@@ -62,96 +66,107 @@ class Db {
 	/* Constructor */
 	/**
 	 * Database connection
+	 *
 	 * @var PDO
 	 */
 	protected $db;
 	/**
 	 * Latest query statement
+	 *
 	 * @var PDOStatement
 	 */
 	protected $result;
 	/**
 	 * Database information
+	 *
 	 * @var stdClass
 	 */
 	protected $info;
 	/**
 	 * Statements cache
+	 *
 	 * @var array
 	 */
 	protected $statement = array();
 	/**
 	 * Tables shema information cache
+	 *
 	 * @var array
 	 */
 	protected $table     = array();
 	/**
 	 * Primary keys information cache
+	 *
 	 * @var array
 	 */
 	protected $key       = array();
 	/**
 	 * Constructor
+	 *
 	 * @uses  PDO
 	 * @throw PDOException
-	 * @param string $driver   Database driver
-	 * @param string $host     Database host
-	 * @param string $database Database name
-	 * @param string $user     User name
-	 * @param string $pass     [Optional] User password
+	 * @param string  $driver   Database driver
+	 * @param string  $host     Database host
+	 * @param string  $database Database name
+	 * @param string  $user     User name
+	 * @param string  $pass     [Optional] User password
 	 * @see   http://php.net/manual/fr/pdo.construct.php
 	 * @todo  Support port/socket within DSN?
 	 */
-	public function __construct ( $driver, $host, $database, $user, $password = null ) {
+	public function __construct( $driver, $host, $database, $user, $password = null ) {
 		set_exception_handler( array( __CLASS__, 'safe_exception' ) );
 		$this->db = new pdo( $driver . ':host=' . $host . ';dbname=' . $database, $user, $password, array(
-			PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-		) );
+				PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+			) );
 		restore_exception_handler();
 		$this->info = (object) array_combine( static::$arguments, func_get_args() );
-		unset($this->info->password);
+		unset( $this->info->password );
 	}
 	/**
 	 * Avoid exposing exception informations
+	 *
 	 * @param Exception $exception [Optional] User password
 	 */
-	public static function safe_exception ( Exception $exception ) {
+	public static function safe_exception( Exception $exception ) {
 		die( 'Uncaught exception: ' . $exception->getMessage() );
 	}
 	/* SQL query */
 	/**
 	 * Get latest SQL query
+	 *
 	 * @return string Latest SQL query
 	 */
-	public function __toString () {
-		return $this->result ? 
+	public function __toString() {
+		return $this->result ?
 			$this->result->queryString :
 			null;
 	}
 	/* Query methods */
 	/**
 	 * Execute raw SQL query
+	 *
 	 * @uses   PDO::query
 	 * @throw  PDOException
-	 * @param  string $sql Plain SQL query
+	 * @param string  $sql Plain SQL query
 	 * @return Db     Self instance
 	 * @todo   ? detect USE query to update dbname ?
 	 */
-	public function raw ( $sql ) {
+	public function raw( $sql ) {
 		$this->result = $this->db->query( $sql );
 		return $this;
 	}
 	/**
 	 * Execute SQL query with paramaters
+	 *
 	 * @uses   PDO::prepare
 	 * @uses   self::_uncomment
 	 * @uses   PDOStatement::execute
 	 * @throw  PDOException
-	 * @param  string $sql    SQL query with placeholder
-	 * @param  array  $params SQL parameters to escape (quote)
+	 * @param string  $sql    SQL query with placeholder
+	 * @param array   $params SQL parameters to escape (quote)
 	 * @return Db     Self instance
 	 */
-	public function query ( $sql, array $params ) {
+	public function query( $sql, array $params ) {
 		$this->result = isset( $this->statement[ $sql ] ) ?
 			$this->statement[ $sql ] :
 			$this->statement[ $sql ] = $this->db->prepare( self::_uncomment( $sql ) );
@@ -160,17 +175,18 @@ class Db {
 	}
 	/**
 	 * Execute SQL select query
+	 *
 	 * @uses   PDO::query
 	 * @throw  PDOException
-	 * @param  string       $table  
-	 * @param  string|array $fields [Optional] 
-	 * @param  string|array $where  [Optional] 
-	 * @param  string       $order  [Optional] 
-	 * @param  string|int   $limit  [Optional] 
+	 * @param string  $table
+	 * @param string|array $fields [Optional]
+	 * @param string|array $where  [Optional]
+	 * @param string  $order  [Optional]
+	 * @param string|int $limit  [Optional]
 	 * @return Db     Self instance
 	 * @todo   Need complete review
 	 */
-	public function select ( $table, $fields = '*', $where = null, $order = null, $limit = null ) {
+	public function select( $table, $fields = '*', $where = null, $order = null, $limit = null ) {
 		$sql = 'SELECT ' . self::_fields( $fields ) . ' FROM ' . $this->_table( $table );
 		if ( $where && $where = $this->_conditions( $where ) )
 			$sql .= ' WHERE ' . $where->sql;
@@ -185,31 +201,34 @@ class Db {
 	/* Query formating helpers */
 	/**
 	 * Check if data is a plain key (without SQL logic)
-	 * @param  mixed $data Data to check
+	 *
+	 * @param mixed   $data Data to check
 	 * @return bool
 	 */
-	static protected function _is_plain ( $data ) {
+	static protected function _is_plain( $data ) {
 		if ( ! is_scalar( $data ) )
 			return false;
 		return is_string( $data ) ? ! preg_match( '/\W/i', $data ) : true;
 	}
 	/**
 	 * Check if array is a simple indexed list
-	 * @param  array $array Array to check
+	 *
+	 * @param array   $array Array to check
 	 * @return bool
 	 */
-	static protected function _is_list ( array $array ) {
+	static protected function _is_list( array $array ) {
 		foreach ( array_keys( $array ) as $key )
 			if ( ! is_int( $key ) )
 				return false;
-		return true;
+			return true;
 	}
 	/**
 	 * Remove all (inline & multiline bloc) comments from SQL query
-	 * @param  string $sql SQL query string
+	 *
+	 * @param string  $sql SQL query string
 	 * @return string SQL query string without comments
 	 */
-	static protected function _uncomment ( $sql ) {
+	static protected function _uncomment( $sql ) {
 		/* '@
 		(([\'"`]).*?[^\\\]\2) # $1 : Skip single & double quoted expressions
 		|(                    # $3 : Match comments
@@ -229,31 +248,33 @@ class Db {
 	}
 	/**
 	 * Format query parameters
+	 *
 	 * @uses   self::_escape
-	 * @param  string|array $data     Data to format
-	 * @param  string       $operator [Optional] 
-	 * @param  string       $glue     [Optional] 
+	 * @param string|array $data     Data to format
+	 * @param string  $operator [Optional]
+	 * @param string  $glue     [Optional]
 	 * @return string       SQL params query chunk
 	 * @todo   Handle integer keys like in self::_conditions
 	 */
-	static protected function _params ( $data, $operator = '=', $glue = ', ' ) {
-		$params = is_string( $data) ? array( $data ) : array_keys( (array) $data );
+	static protected function _params( $data, $operator = '=', $glue = ', ' ) {
+		$params = is_string( $data ) ? array( $data ) : array_keys( (array) $data );
 		foreach ( $params as &$param )
 			$param = implode( ' ', array( self::_escape( $param ), $operator, ':' . $param ) );
 		return implode( $glue, $params );
 	}
 	/**
 	 * Format query fields
+	 *
 	 * @uses   self::_is_plain
-	 * @param  string  $field Field String
+	 * @param string  $field Field String
 	 * @return string  SQL field query chunk
 	 */
-	static protected function _escape ( $field ) {
+	static protected function _escape( $field ) {
 		return self::_is_plain( $field ) ?
 			'`' . $field  . '`' :
 			$field;
 	}
-	static protected function _extract ( $table, $type = 'table' ) {
+	static protected function _extract( $table, $type = 'table' ) {
 		static $infos = array(
 			'database' => '@(?:(`?)(?P<database>\w+)\g{-2})\.(`?)(?P<table>\w+)\g{-2}(?:\.(`?)(?P<field>\w+)\g{-2})?@'
 			, 'table'  => '@(?:(`?)(?P<database>\w+)\g{-2}\.)?(?:(`?)(?P<table>\w+)\g{-2})(?:\.(`?)(?P<field>\w+)\g{-2})?@'
@@ -264,12 +285,12 @@ class Db {
 		$match = array_filter( array_intersect_key( $match, $infos ) );
 		return $match[ $type ];
 	}
-	static protected function _alias ( array $alias ) {
+	static protected function _alias( array $alias ) {
 		foreach ( $alias as $k => $v )
 			$_alias[] = self::_escape( $v ) . ( is_string( $k ) ? ' AS '. self::_escape( $k ) : '' );
 		return $_alias;
 	}
-	static protected function _fields ( $fields ) {
+	static protected function _fields( $fields ) {
 		if ( empty( $fields ) )
 			return '*';
 		if ( is_string( $fields ) )
@@ -277,7 +298,7 @@ class Db {
 		return implode( ', ', self::_alias ( $fields ) );
 	}
 	//@todo
-	static protected function _conditions ( array $conditions ) {
+	static protected function _conditions( array $conditions ) {
 		$sql    = array();
 		$params = array();
 		$i      = 0;
@@ -296,52 +317,52 @@ class Db {
 				$condition = $param;
 			$sql[] = $condition;
 		}
-		return (object) array( 
+		return (object) array(
 			'sql'    => '( ' . implode( ' ) AND ( ', $sql ) . ' )',
 			'params' => $params
 		);
 	}
-	protected function _table ( $table, $escape = true ) {
+	protected function _table( $table, $escape = true ) {
 		return $escape ?
 			self::_escape( $this->_database( $table ) )  . '.' . self::_escape( self::_extract( $table, 'table' )  ) :
 			$this->_database( $table )  . '.' . self::_extract( $table, 'table' );
 	}
-	protected function _database ( $table = null ) {
-		return self::_extract( $table, 'database' ) ?: 
-			$this->info->database;
+	protected function _database( $table = null ) {
+		return self::_extract( $table, 'database' ) ?:
+		$this->info->database;
 	}
 	/* Data column helpers */
-	static protected function _column ( array $data, $field ) {
+	static protected function _column( array $data, $field ) {
 		$column = array();
 		foreach ( $data as $key => $row )
 			if ( is_object( $row ) && isset( $row->{$field} ) )
-				$column[ $key ] = $row->{$field};
-			else if ( is_array( $row ) && isset( $row[ $field ] ) )
+			$column[ $key ] = $row->{$field};
+		else if ( is_array( $row ) && isset( $row[ $field ] ) )
 				$column[ $key ] = $row[ $field ];
-			else 
+			else
 				$column[ $key ] = null;
-		return $column;
+			return $column;
 	}
-	static protected function _index ( array $data, $field ) {
+	static protected function _index( array $data, $field ) {
 		return array_combine(
 			self::_column( $data, $field ),
 			$data
 		);
 	}
 	/* CRUD methods */
-	public function create ( $table, array $data ) {
+	public function create( $table, array $data ) {
 		$keys = array_keys( $data );
 		$sql  = 'INSERT INTO ' . $this->_table( $table ) . ' (' . implode( ', ', $keys ) . ') VALUES (:' . implode( ', :', $keys ) . ')';
-		return $this->query( $sql, $data );  
+		return $this->query( $sql, $data );
 	}
-	//public function read ( $table, $where ) 
-	public function read ( $table, $id, $key = null ) {
+	//public function read ( $table, $where )
+	public function read( $table, $id, $key = null ) {
 		$key = $key ?: current( $this->key( $table ) );
 		$sql = 'SELECT * FROM ' . $this->_table( $table ) . ' WHERE ' . self::_params( $key );
 		return $this->query( $sql, array( ':' . $key => $id ) );
 	}
 	//public function update ( $table, $data, $where )
-	public function update ( $table, $data, $value = null, $id = null, $key = null ) {
+	public function update( $table, $data, $value = null, $id = null, $key = null ) {
 		if ( is_array( $data ) ) {
 			$key  = $id;
 			$id   = $value;
@@ -354,27 +375,27 @@ class Db {
 		return $this->query( $sql, array_merge( $data, array( ':' . $key => $id ) ) );
 	}
 	//public function delete ( $table, $where )
-	public function delete ( $table, $id, $key = null ) {
+	public function delete( $table, $id, $key = null ) {
 		$key = $key ?: current( $this->key( $table ) );
 		$sql = 'DELETE FROM ' . $this->_table( $table ) . ' WHERE ' . self::_params( $key );
 		return $this->query( $sql, array( ':' . $key => $id ) );
 	}
 	/* Fetch methods */
-	public function fetch ( $class = null ) {
+	public function fetch( $class = null ) {
 		if ( ! $this->result )
 			throw new Exception( 'Can\'t fetch result if no query!' );
 		return $class === false ?
 			$this->result->fetch( PDO::FETCH_ASSOC ) :
 			$this->result->fetchObject( $class ?: self::config( 'fetch' ) );
 	}
-	public function all ( $class = null ) {
+	public function all( $class = null ) {
 		if ( ! $this->result )
 			throw new Exception( 'Can\'t fetch results if no query!' );
 		return $class === false ?
 			$this->result->fetchAll( PDO::FETCH_ASSOC ) :
 			$this->result->fetchAll( PDO::FETCH_CLASS, $class ?: self::config( 'fetch' ) );
 	}
-	public function column ( $field, $index = null ) {
+	public function column( $field, $index = null ) {
 		$data   = $this->all( false );
 		$values = self::_column( $data, $field );
 		return is_string( $index ) ?
@@ -382,34 +403,34 @@ class Db {
 			$values;
 	}
 	/* Table infos */
-	public function key ( $table ) {
+	public function key( $table ) {
 		$table = $this->_table( $table, false );
 		if ( self::config( $table . ':PK' ) )
 			return self::config( $table . ':PK' );
 		else if ( isset( $this->key[ $table ] ) )
-			return $this->key[ $table ];
-		$keys = array_keys( self::_column( $this->fields( $table ), 'key' ), 'PRI' );
+				return $this->key[ $table ];
+			$keys = array_keys( self::_column( $this->fields( $table ), 'key' ), 'PRI' );
 		if ( empty( $keys ) )
 			throw new Exception( 'No primary key on ' . $this->_table( $table ) . ' table, please set a primary key' );
 		return $this->key[ $table ] = $keys;
 	}
-	public function fields ( $table ) {
+	public function fields( $table ) {
 		$table = $this->_table( $table, false );
 		if ( isset( $this->table[ $table ] ) )
 			return $this->table[ $table ];
-		$sql = 'SELECT 
-				`COLUMN_NAME`                                               AS `name`, 
-				`COLUMN_DEFAULT`                                            AS `default`, 
-				NULLIF( `IS_NULLABLE`, "NO" )                               AS `null`, 
-				`DATA_TYPE`                                                 AS `type`, 
-				COALESCE( `CHARACTER_MAXIMUM_LENGTH`, `NUMERIC_PRECISION` ) AS `length`, 
-				`CHARACTER_SET_NAME`                                        AS `encoding`, 
-				`COLUMN_KEY`                                                AS `key`, 
-				`EXTRA`                                                     AS `auto`, 
+		$sql = 'SELECT
+				`COLUMN_NAME`                                               AS `name`,
+				`COLUMN_DEFAULT`                                            AS `default`,
+				NULLIF( `IS_NULLABLE`, "NO" )                               AS `null`,
+				`DATA_TYPE`                                                 AS `type`,
+				COALESCE( `CHARACTER_MAXIMUM_LENGTH`, `NUMERIC_PRECISION` ) AS `length`,
+				`CHARACTER_SET_NAME`                                        AS `encoding`,
+				`COLUMN_KEY`                                                AS `key`,
+				`EXTRA`                                                     AS `auto`,
 				`COLUMN_COMMENT`                                            AS `comment`
 			FROM `INFORMATION_SCHEMA`.`COLUMNS`
-			WHERE 
-				`TABLE_SCHEMA` = ' . $this->quote( self::_database( $table ) ) . ' AND 
+			WHERE
+				`TABLE_SCHEMA` = ' . $this->quote( self::_database( $table ) ) . ' AND
 				`TABLE_NAME` = ' . $this->quote( self::_extract( $table )  ) . '
 			ORDER BY `ORDINAL_POSITION` ASC';
 		$fields = $this->db->query( $sql );
@@ -418,22 +439,22 @@ class Db {
 		return $this->table[ $table ] = self::_index( $fields->fetchAll( PDO::FETCH_CLASS ), 'name' );
 	}
 	/* Quote Helper */
-	public function quote ( $value ) {
+	public function quote( $value ) {
 		return is_null( $value ) ?
-			'NULL' : 
+			'NULL' :
 			$this->db->quote( $value );
 	}
-	public function database ( $table = null ) {
-		return $this->_table( $table, true ) ?: 
-			$this->info->database;
+	public function database( $table = null ) {
+		return $this->_table( $table, true ) ?:
+		$this->info->database;
 	}
 	/* Statement infos */
-	public function id () {
+	public function id() {
 		// !! see http://php.net/manual/fr/pdo.lastinsertid.php
 		return $this->db->lastInsertId();
 	}
-	public function count () {
-		return $this->result ? 
+	public function count() {
+		return $this->result ?
 			$this->result->rowCount() :
 			null;
 	}
